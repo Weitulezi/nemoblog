10978cff93c4a2781ace06a422ff11b79a49f294
dixon vande <vandenemo@yahoo.com>
2020-10-28 13:31:44 +0700

    add

--- static_root/js/global.js
+++ static_root/js/global.js
@@ -1,1 +1,30 @@
 $("body").hide().fadeIn(1000)
+
+// $(".curtainButton").click(function () {
+//     const bigBoxContainer = document.querySelector(".bigBoxContainer")
+//     let bigBox = `
+//         <div class="bigBox">
+//             <div class="link">
+//                 <a href="/writing">Writing</a>
+//             </div>
+//             <div class="link">
+//                 <a href="/comic">Comic</a>
+//             </div>
+//             <div class="link">
+//                 <a href="/drawing">Drawing</a>
+//             </div>
+//             <div class="link">
+//                 <a href="/about">About</a>
+//             </div>
+//         </div>
+//     `
+//     bigBoxContainer.innerHTML = bigBox
+//     $(".bigBoxContainer").slideDown(1000)
+//     bigBoxContainer.style.display = "flex"
+//     bigBoxContainer.innerHTML += `<div class="closeBigBox">&#x2716;</div>`
+//     $(".closeBigBox").fadeIn(1000)
+//     $(".closeBigBox").click(function() {
+//         $(".bigBoxContainer").slideUp(1000)
+//         $(".closeBigBox").fadeOut(800)
+//     })
+// })
--- static_root/js/writing.js
+++ static_root/js/writing.js
@@ -1,3 +1,32 @@
+$(".curtainButton").click(function () {
+    const bigBoxContainer = document.querySelector(".bigBoxContainer")
+    let bigBox = `
+        <div class="bigBox">
+            <div class="link">
+                <a href="/writing">Writing</a>
+            </div>
+            <div class="link">
+                <a href="/comic">Comic</a>
+            </div>
+            <div class="link">
+                <a href="/drawing">Drawing</a>
+            </div>
+            <div class="link">
+                <a href="/about">About</a>
+            </div>
+        </div>
+    `
+    bigBoxContainer.innerHTML = bigBox
+    $(".bigBoxContainer").slideDown(1000)
+    bigBoxContainer.style.display = "flex"
+    bigBoxContainer.innerHTML += `<div class="closeBigBox">&#x2716;</div>`
+    $(".closeBigBox").fadeIn(1000)
+    $(".closeBigBox").click(function() {
+        $(".bigBoxContainer").slideUp(1000)
+        $(".closeBigBox").fadeOut(800)
+    })
+})
+
 function loadWriting() {
     fetch(`/api/writing_list`)
     .then(res => res.json())
